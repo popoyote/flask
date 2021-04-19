b96c30d2023d9bce0e09390fcc2170f4f020fbab
popoyote <75493535+popoyote@users.noreply.github.com>
2021-04-19 09:21:42 +0300

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"{self.title}"
+
+
+@app.route('/')
+def index():
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+        item = Item(title=title, price=price)
+        if title != '' and price != '':
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        else:
+            return "Ошибка: вы ввели неккоректные данные"
+    else:
+        return render_template('create.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
